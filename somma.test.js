4b3f769594a8f315b0df36fe54c2d04a89f10e4d
ElisaGotelli <s5666586@studenti.unige.it>
2025-04-14 17:48:07 +0200

    modifica post errore

--- somma.test.js
+++ somma.test.js
@@ -5,7 +5,7 @@ test('correct test: adds 2+2 to equal 4', () => {
     expect(risultato).toBe(4); 
 });
 
-test('wrong test: adds 1+2 to equals 4', () => {
+test('wrong test: adds 3+5 to equals 7', () => {
     const risultato= somma(3, 5); 
     expect(risultato).toBe(7);  
 });
