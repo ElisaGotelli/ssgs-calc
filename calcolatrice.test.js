7426dcc174dcafe2fb59453d38ed20db07d96f56
Elisa <150169637+ElisaGotelli@users.noreply.github.com>
2025-04-17 17:04:23 +0200

    Update calcolatrice.test.js

--- calcolatrice.test.js
+++ calcolatrice.test.js
@@ -1,12 +1,37 @@
-const {somma} = require('./calcolatrice'); 
-
-test('correct test: adds 2+2 to equal 4', () => {
-    const risultato= somma(2, 2); 
+const { somma, sottrazione, moltiplicazione, divisione,potenza} = require('./calcolatrice');
+let risultato; 
+//test sulla somma 
+test('sum correct test: adds 2+2 to equal 4', () => {
+    risultato= somma(2, 2); 
     expect(risultato).toBe(4); 
 });
 
-//il test successivo verrà ignorato perchè errato
-test.skip('wrong test: adds 3+5 to equals 7', () => {
-    const risultato = somma(3, 5); 
+//il test successivo verrà ignorato perchè errato 
+test.skip('sum wrong test: adds 3+5 to equals 7', () => {
+    risultato = somma(3, 5); 
     expect(risultato).toBe(7);  
+});
+
+//test sulla sottrazione 
+test('subtraction test: subtract 4-2 to equal 2', () => {
+    risultato= sottrazione(4, 2); 
+    expect(risultato).toBe(2); 
+});
+
+//test sulla moltiplicazione 
+test('multiplication test: multiply 2*3 to equal 6', () => {
+    risultato= moltiplicazione(2, 3); 
+    expect(risultato).toBe(6); 
+});
+
+//test sulla divisione 
+test('division test:  divides 6/3 to equal 2', () => {
+    risultato= divisione(6, 3); 
+    expect(risultato).toBe(2); 
+});
+
+//test sull'elevamento a potenza
+test('exponentiation test: raise 6 to the power of 2 equals 36', () => {
+    risultato= potenza(6, 2); 
+    expect(risultato).toBe(36); 
 });
