6607edf2f28021125170aa59f3dccd0236ea2be7
Elisa <150169637+ElisaGotelli@users.noreply.github.com>
2025-04-18 22:53:08 +0200

    Update calcolatrice.js

--- calcolatrice.js
+++ calcolatrice.js
@@ -1,21 +1,31 @@
+function numControl(a,b){
+    if(isNan(a) || isNan(b)) throw new Error("uno o entrambi i valori inseriti non sono dei numeri!");
+    return; 
+}
+
 function somma(a, b) {
+    numControl(a,b);
     return a + b;
   }
   
   function sottrazione(a,b){
+    numControl(a,b);
     return a-b;
   }
 
   function moltiplicazione(a,b){
+    numControl(a,b);
     return a*b; 
   }
 
   function divisione(a, b) {
     if (b === 0) throw new Error("Impossibile dividere per zero");
+    numControl(a,b);
     return a / b;
 }
 
   function potenza(a,b){
+    numControl(a,b);
     return  Math.pow(a,b)
 }
 
