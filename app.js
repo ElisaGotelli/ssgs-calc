d86d56be545daf129f50ea5bf52d0c2d2f4eba76
Elisa <150169637+ElisaGotelli@users.noreply.github.com>
2025-04-19 15:39:46 +0200

    Update app.js

--- app.js
+++ app.js
@@ -7,53 +7,65 @@ const rl = readline.createInterface({
   });
 
 console.log('Benvenuto nella Calcolatrice!'); 
-console.log('Di quale operazione vuoi usufruire?'); 
-console.log('- scrivi 1 per Addizione');
-console.log('- scrivi 2 per Sottrazione');
-console.log('- scrivi 3 per Moltiplicazione');
-console.log('- scrivi 4 per Divisione');
-console.log('- scrivi 5 per elevare a Potenza');
+let risultato, continuo; 
+OperazioneSingola(); 
 
-rl.question("Digita il numero dell’operazione (ATTENZIONE!: il programma è case sensitive! NON INSERIRE SPAZI): ", function(op) {
-    rl.question("Inserisci il primo operatore: ", function(num1) {
-        rl.question("Inserisci il secondo operatore: ", function(num2) {
-            const a = parseFloat(num1);
-            const b = parseFloat(num2);
-            
-            let risultato;
-            try {
-                switch(op) {
-                    case '1': 
-                        risultato = somma(a, b);
-                        console.log(`Il risultato della somma è: ${risultato}`);
-                        break;
-                    case '2': 
-                        risultato = sottrazione(a, b); 
-                        console.log(`Il risultato della sottrazione è: ${risultato}`);
-                        break;
-                    case '3': 
-                        risultato = moltiplicazione(a, b); 
-                        console.log(`Il risultato della moltiplicazione è: ${risultato}`);
-                        break;
-                    case '4': 
-                        risultato = divisione(a, b); 
-                        console.log(`Il risultato della divisione è: ${risultato}`);
-                        break;
-                    case '5': 
-                        risultato = potenza(a, b); 
-                        console.log(`Il risultato dellelevamento a potenza è: ${risultato}`);
-                        break;
-                    default:
-                        console.log("Errore! Inserito un numero di operazione non valido!");
-                        rl.close();
-                        return;
+function OperazioneSingola(){
+    console.log('Di quale operazione vuoi usufruire?'); 
+    console.log('- scrivi 1 per Addizione');
+    console.log('- scrivi 2 per Sottrazione');
+    console.log('- scrivi 3 per Moltiplicazione');
+    console.log('- scrivi 4 per Divisione');
+    console.log('- scrivi 5 per elevare a Potenza');
+    
+    rl.question("Digita il numero dell’operazione (ATTENZIONE!: il programma è case sensitive! NON INSERIRE SPAZI): ", function(op) {
+        rl.question("Inserisci il primo operatore: ", function(num1) {
+            rl.question("Inserisci il secondo operatore: ", function(num2) {
+                const a = parseFloat(num1);
+                const b = parseFloat(num2);
+                
+                try {
+                    switch(op) {
+                        case '1': 
+                            risultato = somma(a, b);
+                            console.log(`Il risultato della somma è: ${risultato}`);
+                            break;
+                        case '2': 
+                            risultato = sottrazione(a, b); 
+                            console.log(`Il risultato della sottrazione è: ${risultato}`);
+                            break;
+                        case '3': 
+                            risultato = moltiplicazione(a, b); 
+                            console.log(`Il risultato della moltiplicazione è: ${risultato}`);
+                            break;
+                        case '4': 
+                            risultato = divisione(a, b); 
+                            console.log(`Il risultato della divisione è: ${risultato}`);
+                            break;
+                        case '5': 
+                            risultato = potenza(a, b); 
+                            console.log(`Il risultato dellelevamento a potenza è: ${risultato}`);
+                            break;
+                        default:
+                            console.log("Errore! Inserito un numero di operazione non valido!");
+                            rl.close();
+                            return;
+                    }
+                } catch (err) {
+                    console.log("Errore!", err.message);
                 }
-            } catch (err) {
-                console.log("Errore!", err.message);
-            } finally {
-                console.log("Arrivederci!");
-                rl.close();
-            }
+                Continuazione(); 
+            });
         });
     });
-});
+}
+
+function Continuazione(){ 
+    rl.question("Vuoi continuare ad usare la calcolatrice? Scrivere Y se sì sennò N", function(continue) {
+        if(continue == Y) OperazioneSingola(); 
+        else{
+            console.log("Fine utilizzo calcolatrice. Arrivederci!"); 
+            r1.close(); 
+        }
+    });      
+}  
